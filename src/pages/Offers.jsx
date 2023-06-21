e83ae546b1e73bfbc2147c5bd8ba8bfc67416425
Jude Idoko <judidoko@gmail.com>
2023-06-21 22:12:55 +0100

    Real Estate Project: Creating Reat App and Adding Routes

---
+++ src/pages/Offers.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Offers = () => {
+  return (
+    <>
+      <div>
+        <h2>offers page</h2>
+      </div>
+    </>
+  );
+};
+
+export default Offers;
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SignUp = () => {
+  return (
+    <>
+      <div>
+        <h2>SignUpp Page</h2>
+      </div>
+    </>
+  );
+};
+
+export default SignUp;
