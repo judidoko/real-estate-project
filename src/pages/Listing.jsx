a3247db6b339311454db02d0d30fa95504765032
Jude Idoko <judidoko@gmail.com>
2023-10-24 22:30:33 +0100

    add share button and it functionality

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -7,6 +7,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore from "swiper";
 import { EffectFade, Autoplay, Navigation, Pagination } from "swiper/modules";
 import "swiper/css/bundle";
+import { FaShare } from "react-icons/fa";
 
 const Listing = () => {
   const params = useParams();
@@ -15,6 +16,7 @@ const Listing = () => {
   // UseState Hooks
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [shareLinkCopied, setShareLinkCopied] = useState(false);
 
   // useEffect to fetch the listing from firbase store
   useEffect(() => {
@@ -47,12 +49,29 @@ const Listing = () => {
               className="relative w-full h-[400px] overflow-hidden"
               style={{
                 background: `url(${listing.imgUrls[index]}) center no-repeat`,
-                backgroundSize: "cover ",
+                backgroundSize: "cover",
               }}
             ></div>
           </SwiperSlide>
         ))}
       </Swiper>
+      <div
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href);
+          setShareLinkCopied(true);
+          setTimeout(() => {
+            setShareLinkCopied(false);
+          }, 2000);
+        }}
+        className="fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center"
+      >
+        <FaShare className="text-lg text-slate-500" />
+      </div>
+      {shareLinkCopied && (
+        <p className="fixed top-[24%] right-[5%] font-semibold border-2 border-gray-400 rounded-md bg-white z-10 p-2">
+          Link Copied
+        </p>
+      )}
     </main>
   );
 };
